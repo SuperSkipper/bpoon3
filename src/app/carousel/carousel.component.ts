7916f1a9dbd36446b4c6f3f6df25546fe08f7e73
Ben Poon <ben@bpoon.com>
2025-02-16 01:22:00 -0600

    Project carousel part 3

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 import CarouselItem from '../../models/carouselItem.model';
 import {CommonModule} from '@angular/common';
+import {fromEvent, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
@@ -12,10 +13,51 @@ import {CommonModule} from '@angular/common';
 })
 export class CarouselComponent {
   @Input() public carouselItems: CarouselItem[] = [];
-
+  @ViewChild('images') images!: ElementRef;
+  private scrollSubscription!: Subscription;
+  private tapSubscription!: Subscription;
   constructor() {
 
   }
 
-  protected readonly CarouselItem = CarouselItem;
+  ngAfterViewInit() {
+    this.images.nativeElement.scrollBy(1,0);
+    this.scrollSubscription = fromEvent(this.images.nativeElement, 'wheel').subscribe((event) => {
+      if (event instanceof Event) {
+        event.preventDefault();
+      }
+    })
+    this.tapSubscription = fromEvent(this.images.nativeElement, 'touchmove').subscribe((event) => {
+      if (event instanceof Event) {
+        console.log(event);
+        event.preventDefault();
+      }
+    })
+  }
+
+  scroll(direction: string) {
+    switch (direction) {
+      case 'left':
+        this.images.nativeElement.scrollBy({
+          top: 0,
+          left: -10,
+          behavior: 'smooth'
+        });
+        break;
+      case 'right':
+        this.images.nativeElement.scrollBy({
+          top: 0,
+          left: 10,
+          behavior: 'smooth'
+        });
+        //this.carouselItems.push(this.carouselItems.shift()!);
+        break;
+    }
+  }
+
+  ngOnDestroy() {
+    this.scrollSubscription?.unsubscribe();
+    this.tapSubscription?.unsubscribe();
+  }
+
 }
